5cf0359860bbb55874d52c3dad88472c47f00fb5
Achu-kappithan <achukappithan4236@gmail.com>
2025-05-09 08:36:47 +0530

    Intagrate logout functionality

--- src/app/shared/services/aut-service.service.ts
+++ src/app/shared/services/aut-service.service.ts
@@ -4,22 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AutServiceService {
-  // Save token
+ 
   setToken(token: string) {
     localStorage.setItem('token', token);
   }
 
-  // Get token
+
   getToken(): string | null {
     return localStorage.getItem('token');
   }
 
-  // Remove token (on logout)
+
   removeToken() {
     localStorage.removeItem('token');
   }
 
-  // Check if user is logged in
+ 
   isLoggedIn(): boolean {
     return !!this.getToken(); 
   }
